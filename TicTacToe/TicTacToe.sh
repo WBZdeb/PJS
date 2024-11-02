11813ec5374c5c928a61386f7a65804ed9310215
Wojciech Zdeb <wojciech.zdeb98@gmail.com>
2024-11-02 21:25:06 +0100

    Added array for storing game state, functions for drawing board and legend

--- TicTacToe/TicTacToe.sh
+++ TicTacToe/TicTacToe.sh
@@ -2,6 +2,16 @@
 
 #vars
 player_input=-1
+board_array=(0 0 0 0 0 0 0 0 0)	# 0 -> empty, 1 -> cross, -1 -> nought
+
+array_sum(){
+	tot=0
+
+	for i in ${board_array[@]}; do
+		tot+=${i#-}
+	done
+	echo $tot
+} 
 
 #Perform game setup
 start_game(){
@@ -12,20 +22,46 @@ start_game(){
 main_menu(){
 	clear
 	
-	
-	echo -e -n "=======================================\n"
 	echo -e -n "WELCOME TO TIC-TAC-TOE!\n\n"
-	echo -e -n "1 - Player VS Player\n"
-	echo -e -n "2 - Player VS Computer\n\n"
-	echo -e -n "0 - Exit game\n"
+	echo -e -n "=======================================\n"
+	echo -e -n "PvP - Player VS Player\n"
+	echo -e -n "PvE - Player VS Computer\n\n"
+	echo -e -n "Exit - Exit game\n"
 	echo -e -n "=======================================\n"	
-	echo -e -n "Choose you game mode: "
+	echo -e -n "Select: "
 	
 	read player_input
 	
 	echo -e -n "\n\n You chose: ${player_input}\n"
 }
 
+#Draw game board
+draw_board(){
+	clear
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "---+---+---\n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "---+---+---\n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n"
+	echo -e -n "   |   |   \n\n"
+}
 
-start_game
-main_menu
+display_game_legend(){
+	
+	echo -e -n "=======================================\n"
+	echo -e -n "1-9 - Select space\n\n"
+	echo -e -n "Exit - Exit game\n"
+	echo -e -n "=======================================\n"	
+}
+
+#Main block
+main(){
+	start_game
+	main_menu
+}
+main "$@"
